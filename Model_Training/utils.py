cb549e2575886a1ace598c0a77ff03c782b1010a
dola-11 <sanjogitabhagowati11@gmail.com>
2025-05-31 20:09:50 +0530

    Add utils.py for utility functions

--- Model_Training/utils.py
+++ Model_Training/utils.py
@@ -8,18 +8,18 @@ class ImageProcessor:
     def __init__(self):
         pass
 
-    def preprocess_image(path):
+    def preprocess_image(self,path):
         img = Image.open(path).resize((256, 256))
         img = np.array(img).astype(np.float32)
         img = (img / 127.5) - 1.0
         return np.expand_dims(img, axis=0)
 
-    def display_image(image):
+    def display_image(self,image):
         image = (image[0] + 1.0) / 2.0
         plt.imshow(image)
         plt.axis('off')
         plt.show()
 
-    def save_image(tensor, path):
+    def save_image(self,tensor, path):
         img = (tensor.numpy() * 255).astype(np.uint8)
         Image.fromarray(img).save(path)
