a2635906a6c3a143fa523b5c925a5e618eaf844f
dola-11 <sanjogitabhagowati11@gmail.com>
2025-05-31 20:04:29 +0530

    Add data_loader for training model

--- Model_Training/data_loader.py
+++ Model_Training/data_loader.py
@@ -8,16 +8,16 @@ class ImageDataset:
     def __init__():
        pass
 
-    def decode_image(image_path):
+    def decode_image(self,image_path):
         image = tf.io.read_file(image_path)
         image = tf.image.decode_jpeg(image, channels=3)
         image = tf.image.resize(image, [HEIGHT, WIDTH])
         image = (tf.cast(image, tf.float32) / 127.5) - 1
         return image
 
-    def load_dataset(image_paths, training=True):
+    def load_dataset(self,image_paths, training=True):
         dataset = tf.data.Dataset.from_tensor_slices(image_paths)
-        dataset = dataset.map(decode_image, num_parallel_calls=tf.data.AUTOTUNE)
+        dataset = dataset.map(self.decode_image, num_parallel_calls=tf.data.AUTOTUNE)
         if training:
             dataset = dataset.shuffle(BUFFER_SIZE)
         return dataset.batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
